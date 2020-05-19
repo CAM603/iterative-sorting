36ec7371d526081ba95b7cfbe2c2ce6a79dbe5be
CAM603 <cameronhawley@comcast.net>
2020-05-19 13:01:58 -0400

    add count sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -58,7 +58,13 @@ def selection_sort(arr):
 #     return arr
 
 def bubble_sort(arr):
+    # We want to stop passing through the list
+    # as soon as we pass through without swapping any elements
     swapped = True
+    # largest elements in an iteration end up at the end of the array
+    # the first time we pass through, the position n is the largest element
+    # the second time, n - 1 is the second largest element etc etc
+    # so with each iteration, we can look at one less element than before
     num_of_iterations = 0
     while swapped:
         swapped = False
@@ -70,9 +76,31 @@ def bubble_sort(arr):
         num_of_iterations += 1
     return arr
 # STRETCH: implement the Count Sort function below
+# requires us to know the max value that we will be sorting
+# The max argument is so we can specify the max value
+# Total range of data we are sorting sits between 0 and maximum value
 
 
 def count_sort(arr, maximum=-1):
-    # Your code here
+    if len(arr) == 0:  # O(1)
+        return arr  # O(1)
+    # if max value is not specified, we will take the max vale from the input array
+    if maximum == -1:  # O(1)
+        maximum = max(arr)  # O(n)
+    # make a bunch of buckets
+    buckets = [0 for _ in range(maximum + 1)]  # O(max)
 
+    for x in arr:  # O(n)
+        if x < 0:
+            return 'Error, negative numbers not allowed in Count Sort'
+        buckets[x] += 1  # O(1)
+    # we have the counts of every value in our input array
+    # loop through our buckets, starting at the smallest index
+    j = 0
+    # This loop is reversing the tallying we did in the previous loop
+    for i in range(len(buckets)):  # O(n)
+        while buckets[i] > 0:
+            arr[j] = i
+            j += 1
+            buckets[i] -= 1
     return arr
